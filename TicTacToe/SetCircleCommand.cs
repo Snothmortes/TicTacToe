1032e3cf487e772b129df6071e3eb55d3a56e3f1
glen <glen.jakobsen@gmail.com>
2020-10-10 10:54:40 +0200

    changes

--- TicTacToe/SetCircleCommand.cs
+++ TicTacToe/SetCircleCommand.cs
@@ -7,7 +7,7 @@ namespace Game
     {
         private PictureBox _picBox;
         public SetCircleCommand(PictureBox picBox) => _picBox = picBox;
-        public void ExecuteAction() => _picBox.Image = Image.FromFile(@"C:\Users\Glen\source\repos\TicTacToe\TicTacToe\circle.bmp");
+        public void ExecuteAction() => _picBox.Image = Image.FromFile(@"\..\circle.bmp");
         public void RedoAction() => ExecuteAction();
         public void UndoAction()
         {
